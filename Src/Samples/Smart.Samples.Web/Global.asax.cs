4ace1319df6c05eb668891267daf01f93c9af7b5
ljf <185654894@qq.com>
2016-01-29 23:13:06 +0800

    增加 MiniProfiler

--- Src/Samples/Smart.Samples.Web/Global.asax.cs
+++ Src/Samples/Smart.Samples.Web/Global.asax.cs
@@ -2,6 +2,14 @@
 using Smart.Core;
 using Smart.Core.Configuration;
 using Smart.Core.Infrastructure;
+using StackExchange.Profiling;
+using StackExchange.Profiling.Data;
+using StackExchange.Profiling.EntityFramework6;
+using StackExchange.Profiling.Mvc;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -22,6 +30,19 @@ namespace Smart.Samples.Web
                     DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
                 }
             });
+
+            GlobalFilters.Filters.Add(new ProfilingActionFilter());
+            MiniProfilerEF6.Initialize();
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            MiniProfiler.Start();
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+            MiniProfiler.Stop();
         }
     }
 }
